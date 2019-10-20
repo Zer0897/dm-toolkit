61035b20042ca4c0b7e61d0c58cc02e1f8e67377
Noah <noah@coronasoftware.net>
2019-10-19 21:26:23 -0700

    Update relm to latest version
    Use latest demo example.

--- dnd-toolkit/src/main.rs
+++ dnd-toolkit/src/main.rs
@@ -1,51 +1,51 @@
 mod counter;
 
-use chrono::Local;
+use gtk::prelude::*;
+use gtk::Inhibit;
 use gtk::Orientation::Vertical;
-use gtk::{
-    Button, ButtonExt, ContainerExt, Inhibit, Label, LabelExt, WidgetExt, Window, WindowType,
-};
-use relm::EventStream;
+use relm::{connect, connect_stream, init, Component, Widget};
+use relm_derive::{widget, Msg};
 
+use self::Msg::*;
 
-// There will be several widgets involved in this example, but this struct
-// will act as a container just for the widgets that we will be updating.
-struct Widgets {clock_label: Label,
-                counter_label: Label,
+#[derive(Msg)]
+pub enum Msg {
+    Quit,
 }
 
-// This enum holds the various messages that will be passed between our
-// widgets. Note that we aren't deriving `Msg` because this example uses
-// the `core` module, which is the basic event-handling library that
-// `relm` depends on.
-#[derive(Clone, Debug)]
-enum Msg {
-    Quit
+pub struct Model {
+    counter: Component<counter::Counter>,
 }
 
-// This struct represents the model, and it maintains the state needed to
-// populate the widget. The model is updated in the `update` method.
-struct Model {
-}
+#[widget]
+impl Widget for Win {
+    fn model() -> Model {
+        let counter = init::<counter::Counter>(()).expect("Counter");
 
-
-fn main() {
-    gtk::init().expect("gtk::init failed");
-
-    let window = Window::new(WindowType::Toplevel);
-    let counter_view = counter::init();
-    let main_stream = EventStream::new();
-
-    {
-        let stream = main_stream.clone();
-        window.connect_delete_event(move |_, _| {
-            stream.emit(Msg::Quit);
-            Inhibit(false)
-        });
+        Model { counter }
     }
 
-    window.add(&counter_view);
-    window.show_all();
+    fn update(&mut self, event: Msg) {
+        match event {
+            Quit => gtk::main_quit(),
+        }
+    }
 
-    gtk::main();
+    view! {
+        #[name="window"]
+        gtk::Window {
+            titlebar: Some(self.model.counter.widget()),
+
+            #[name="app"]
+            gtk::Box {
+                orientation: Vertical
+            },
+
+            delete_event(_, _) => (Quit, Inhibit(false)),
+        }
+    }
+}
+
+fn main() {
+    Win::run(()).expect("Window::run");
 }
